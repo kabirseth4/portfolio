80434a127a8a9042202b861d5f025bf6e7f837d0
kabirseth4 <kabirseth4@gmail.com>
2024-04-17 11:38:46 +0100

    Fix: strange tailwind behaviour with z-index

--- src/components/Window/index.tsx
+++ src/components/Window/index.tsx
@@ -9,7 +9,7 @@ interface WindowProps {
 export const Window = ({ children, className, onClick }: WindowProps) => {
   return (
     <div
-      className={`absolute transform resize overflow-hidden rounded-xl border border-gray-600 ${className ? className : ""}`}
+      className={`absolute resize overflow-hidden rounded-xl border border-gray-600 ${className ? className : ""}`}
       onClick={onClick}
     >
       {children}
